745d3388f03c4a3d9baeac33fb77cb01816c3875
Aizan Ahmed <malikaizan198@gmail.com>
2026-02-11 21:17:38 +0500

    feat(v2.1): update configuration files to v2.1

--- config/app.php
+++ config/app.php
@@ -197,7 +197,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        App\Providers\RuntimeOptimizationServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Mews\Captcha\CaptchaServiceProvider::class,
         Jorenvh\Share\Providers\ShareServiceProvider::class,
